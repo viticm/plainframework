7e5a7bfad7004d430b641a9499201a4787d29c63
viticm <viticm.ti@gmail.com>
2014-09-17 17:43:18 +0800

    documentation update

--- applications/pf_simple/src/main.cc
+++ applications/pf_simple/src/main.cc
@@ -1,23 +1,24 @@
-#include "pf/engine/kernel.h"
+#include "pf/db/manager.h"
 #include "main.h"
 
-pf_engine::Kernel g_engine_kernel;
 int32_t main(int32_t argc, char * argv[]) {
-#if __WINDOWS__
-  USE_PARAM(argc);
-  USE_PARAM(argv);
-  _CrtSetDbgFlag(_CrtSetDbgFlag(0) | _CRTDBG_LEAK_CHECK_DF);
-  system("color 02"); //color green
-  system("mode con cols=120"); //cmd size
-  WORD versionrequested;
-  WSADATA data;
-  int32_t error;
-  versionrequested = MAKEWORD(2, 2);
-  error = WSAStartup(versionrequested, &data);
-#endif
-  //g_engine_kernel.setconfig(ENGINE_CONFIG_NET_ISACTIVE, true);
-  g_engine_kernel.init();
-  g_engine_kernel.run();
-  g_engine_kernel.stop();
+  pf_db::Manager db_manager;
+  bool result = true;
+  result = db_manager.init("sword_user", "root", "mysql");
+  if (!result) return 1;
+  if (!db_manager.isready()) return 1;
+  enum {
+    kDBId = 1,
+    kDBName,
+  };
+  db_manager.get_internal_query()
+    ->parse("select id, name from users where name = 'viticm'");
+  db_manager.query();
+  db_manager.fetch();
+  int32_t error_code = 0;
+  char value[64] = {0};
+  int32_t get_code = 
+    db_manager.get_string(kDBName, value, sizeof(value) - 1, error_code);
+  DEBUGPRINTF("get_code: %d, value: %s", get_code, value);
   return 0;
 }
